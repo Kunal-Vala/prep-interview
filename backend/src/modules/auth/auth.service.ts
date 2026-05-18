1b10b0073f9a6139f76c54b8d3692ef4ec0b24df
Kunal Vala <kunalvala2005@gmail.com>
2026-05-18 16:53:42 +0530

    add : AuthService class & register func

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException, ConflictException } from "@nestjs/common";
+import { JwtService } from "@nest/jwt";
+import { PrismaService } from "src/prisma/prisma.service";
+import { ConfigService } from "@nest/config"
+import * as bcrypt from 'bcryptjs'
+import { RegisterDto } from "./dto/register.dto";
+import { LoginDto } from "../users/dto/login.dto";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly jwt: JwtService,
+        private readonly config: ConfigService,
+    ) { }
+
+    async register(dto: RegisterDto) {
+        const exists = await this.prisma.user.findUnique({ where: { email: dto.email } })
+        if (exists) throw new ConflictException('Email Already Registered');
+
+        const passwordHash = await bcrypt.hash(dto.password, 12)
+        const user = await this.prisma.user.create({
+            data: { email: dto.email, passwordHash, displayName: dto.displayName },
+            select: { id: true, email: true, displayName: true, role: true, createdAt: true }
+        })
+
+        const token = await this.issueTokens(user.id, user.email)
+        return { user, ...token }
+    }
+
+    
+}
