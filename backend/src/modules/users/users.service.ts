ccafb0c24d672a6937f1f6e77e552dfaad01b5d9
Kunal Vala <kunalvala2005@gmail.com>
2026-05-18 18:00:49 +0530

    add : findMe (find user by id)

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async findMe(userId: string) {
+        const user = await this.prisma.user.findUnique({
+            where: { id: userId },
+            select: {
+                id: true,
+                email: true,
+                displayName: true,
+                avatarUrl: true,
+                role: true,
+                isVerified: true,
+                createdAt: true,
+                lastLoginAt: true
+            }
+        })
+        if(!user) throw new NotFoundException('User Not Found')
+        return user
+    }
+}
