f6133888982d7bced89ff0d0d498d59201e60da2
Kunal Vala <kunalvala2005@gmail.com>
2026-05-18 18:01:45 +0530

    add : jwt_module import && /users/me route

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/common/guards/ws-jwt.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(
+        private readonly userService: UsersService
+    ) { }
+
+    @Get('me')
+    @UseGuards(JwtAuthGuard)
+    getMe(@Req() req : any){
+        return this.userService.findMe(req.user.sub)
+    }
+}
--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_ACCESS_SECRET')
+      }),
+    }),
+  ],
   providers: [UsersService],
   controllers: [UsersController]
 })
-export class UsersModule {}
+
+export class UsersModule { }
+
