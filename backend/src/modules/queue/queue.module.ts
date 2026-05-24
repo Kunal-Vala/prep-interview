1b162405a8653202c4c3f6fed8efc58a038b4576
Kunal Vala <kunalvala2005@gmail.com>
2026-05-24 15:51:33 +0530

    fix : redis port was Nan

--- backend/src/modules/queue/queue.module.ts
+++ backend/src/modules/queue/queue.module.ts
@@ -9,13 +9,17 @@ import { FeedbackProducer } from './producers/feedback.producer';
 @Module({
   imports: [
     BullModule.forRootAsync({
-      useFactory: (config: ConfigService) => ({
-        connection: {
-          host: config.get('REDIS_HOST', 'localhost'),
-          port: config.get('REDIS_HOST', 6379),
-          password: config.get('REDIS_PASSWORD') || undefined,
-        },
-      }),
+      useFactory: (config: ConfigService) => {
+        const redisPort = Number.parseInt(config.get('REDIS_PORT') ?? '', 10);
+
+        return {
+          connection: {
+            host: config.get('REDIS_HOST', 'localhost'),
+            port: Number.isNaN(redisPort) ? 6379 : redisPort,
+            password: config.get('REDIS_PASSWORD') || undefined,
+          },
+        };
+      },
       inject: [ConfigService],
     }),
     BullModule.registerQueue({ name: FEEDBACK_QUEUE }, { name: RESUME_QUEUE }),
