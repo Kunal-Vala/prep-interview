bb20f69c22b8c29b10b7c3dca3a5edc7bd786a29
Kunal Vala <kunalvala2005@gmail.com>
2026-06-04 13:42:05 +0530

    add: session dto

---
+++ backend/src/modules/interview/dto/create-session.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, Min, Max, IsIn, IsInt } from 'class-validator';
+
+export class CreateSessionDto {
+  @IsString()
+  targetRole!: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  difficulty!: number;
+
+  @IsString()
+  @IsIn(['TEXT', 'VOICE', 'VIDEO_SIM'])
+  mode!: 'TEXT' | 'VOICE' | 'VIDEO_SIM';
+}
